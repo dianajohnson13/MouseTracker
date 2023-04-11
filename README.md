ecc1706f351ec3b0400d65c430c56a266af70b56
Diana <dianajohnson13@gmail.com>
2023-04-11 09:09:15 -0700

    Change variable name from tail to marker; add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mouse Tracker
+
+A Chrome extension that provides additional visual feedback identifying the user's cursor position. Intended for users with low vision. 
+
+Next Steps:
+ - Provide the user with alternative `marker` style options
+ - TBD
--- content.js
+++ content.js
@@ -1,25 +1,25 @@
-let tail = document.createElement("div");
+let marker = document.createElement("div");
 
-tail.setAttribute("id", "tail");
+marker.setAttribute("id", "marker");
 
 let size = 60;
 
-tail.style.background = "transparent";
-tail.style.height = size + "px";
-tail.style.width = size + "px";
-tail.style.borderRadius = "50%";
-tail.style.border = "4px solid red";
-tail.style.position = "fixed";
-tail.style.zIndex = "1000";
-tail.style.pointerEvents = "none";
+marker.style.background = "transparent";
+marker.style.height = size + "px";
+marker.style.width = size + "px";
+marker.style.borderRadius = "50%";
+marker.style.border = "4px solid red";
+marker.style.position = "fixed";
+marker.style.zIndex = "1000";
+marker.style.pointerEvents = "none";
 
-document.body.append(tail);
+document.body.append(marker);
 
 document.addEventListener('mousemove', function(e) {
-    let tail = document.getElementById('tail');
+    let marker = document.getElementById('marker');
     let left = e.clientX;
     let top = e.clientY;
-    tail.style.left = left - (size / 2) + 'px';
-    tail.style.top = top - (size / 2) + 'px';
+    marker.style.left = left - (size / 2) + 'px';
+    marker.style.top = top - (size / 2) + 'px';
   });
 
